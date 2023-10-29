532aca946f866633d59c79df1d98fee0bddd9510
Seb <seblemery@gmail.com>
2023-10-29 15:58:17 -0400

    Update client.lua
    forgot to edit a variable

--- client.lua
+++ client.lua
@@ -7,6 +7,6 @@ Citizen.CreateThread(function()
     end
 
     -- Replace the parachute texture with the one you want
-    local blackTexture = "parachute_texture.png"
-    SetParachuteTextureVariation(parachuteModel, blackTexture)
+    local chuteTexture = "parachute_texture.png"
+    SetParachuteTextureVariation(parachuteModel, chuteTexture)
 end)
